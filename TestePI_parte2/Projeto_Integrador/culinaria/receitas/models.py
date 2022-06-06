d63dcc77db4262fea958da9c802babbbf447185c
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-06-05 23:01:23 -0300

    Teste django 2

--- TestePI_parte2/Projeto_Integrador/culinaria/receitas/models.py
+++ TestePI_parte2/Projeto_Integrador/culinaria/receitas/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
-class Receita(models.model):
+class Receita(models.Model):
     nome = models.CharField(max_length=20, db_index=True)
     descricao = models.CharField(max_length=500)
 
 
+#def new_func():
     def __str__(self):
         #return super().__str__()
         return self.nome
+
+#return new_func()
